392e4555eedbf0be2e59a52508f8834029404348
Angie Fredrick <angie.fredrick@swiftpage.com>
2014-09-03 14:23:01 -0700

    af: Developer Tips help - hosted

---
+++ Data/SkinSaleslogix Web Developers Reference/Skin.js
@@ -0,0 +1,8 @@
+var xmlSkinData = "";
+xmlSkinData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlSkinData += '<CatapultSkin Version=\"1\" Title=\"Saleslogix Developers Tips\" AutoSyncTOC=\"true\" Tabs=\"TOC,Index,Search,Favorites\" Top=\"25px\" Left=\"710px\" Width=\"620px\" Height=\"650px\" DefaultTab=\"TOC\">';
+xmlSkinData += '    <HtmlHelpOptions ShowMenuBar=\"false\" TopmostWindowStyle=\"false\" AutoShowNavigationPane=\"false\" EnableButtonCaptions=\"false\" AdvancedSearch=\"false\" NavigationPaneWidth=\"215\" Buttons=\"Hide,Back,Options,Print\" HideNavigationOnStartup=\"false\" />';
+xmlSkinData += '    <Toc LinesBetweenItems=\"false\" LinesFromRoot=\"false\" SingleClick=\"true\" PlusMinusSquares=\"false\" AlwaysShowSelection=\"false\" UseFolderIcons=\"false\" ImageListWidth=\"16\" />';
+xmlSkinData += '    <Stylesheet Link=\"Stylesheet.xml\" />';
+xmlSkinData += '</CatapultSkin>';
+CMCXmlParser._FilePathToXmlStringMap.Add('Skin', xmlSkinData);
